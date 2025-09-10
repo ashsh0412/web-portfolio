835eaba426d314dda3ac681b833d324791a7b1c1
Sungho Baek <ashsh0412@gmail.com>
2025-09-10 14:04:27 +0900

    delete color

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const projects = [
     title: "TripCal",
     description:
       "Developed a cross-platform travel budget calculator app using React Native and TypeScript. Integrated public APIs to provide cost of living data, currency conversion, and official travel advisories. Implemented features for personalized budget planning, city cost comparison, and embassy information, while managing the full development lifecycle from UI design to deployment.",
-    color: "linear-gradient(to right, #facc15, #ef4444)",
     tags: ["React Native", "TypeScript", "Expo", "App Store Deployment"],
     year: "Sep 2025 - Present",
     link: "https://apps.apple.com/kr/app/tripcal/id6751708982",
@@ -12,7 +11,6 @@ const projects = [
     title: "DriverMatch",
     description:
       "“Developed a full-stack carpool matching platform leveraging Google Maps API for optimized route planning and real-time matching between drivers and passengers. Designed and implemented both frontend and backend systems, integrated secure authentication, and managed the entire development lifecycle including deployment, monitoring, and performance optimization.”",
-    color: "linear-gradient(to right, #a855f7, #ec4899)",
     tags: ["TypeScript", "Python", "Django", "Google APIs", "OAuth"],
     year: "Oct 2024 - Present",
     link: "https://drivermatch.store",
@@ -21,7 +19,6 @@ const projects = [
     title: "Auto Profanity Translator",
     description:
       "Worked with a team to create a Chrome extension that replaces offensive language with polite alternatives in online content. Leveraged the OpenAI API to detect and convert inappropriate words.",
-    color: "linear-gradient(to right, #60a5fa, #22c55e)",
     tags: ["JavaScript", "OpenAI API", "Chrome Extension"],
     year: "Jun 2023 - Jul 2023",
   },
@@ -29,7 +26,6 @@ const projects = [
     title: "Job Scraper",
     description:
       "Developed a website to automatically gather job listings from major platforms like Stack Overflow and Indeed. Used Beautiful Soup for web scraping and Flask to handle data transfer.",
-    color: "linear-gradient(to right, #60a5fa, #22c55e)",
     tags: ["Python", "JavaScript", "Beautiful Soup", "Flask"],
     year: "Nov 2020 - Dec 2020",
   },
